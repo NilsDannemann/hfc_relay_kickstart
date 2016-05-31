24f83205c289c276c9aa69a749ee8813fb53d355
Nils Dannemann <ndannemann@gmail.com>
2016-05-31 16:36:15 +0200

    [update] implemented checkbox & radiobutton mixins

--- tests/integration/components/hfc-form-radiobutton/component-test.js
+++ tests/integration/components/hfc-form-radiobutton/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('hfc-form-radio', 'Integration | Component | hfc form radio', {
+moduleForComponent('hfc-form-radiobutton', 'Integration | Component | hfc form radiobutton', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{hfc-form-radio}}`);
+  this.render(hbs`{{hfc-form-radiobutton}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#hfc-form-radio}}
+    {{#hfc-form-radiobutton}}
       template block text
-    {{/hfc-form-radio}}
+    {{/hfc-form-radiobutton}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
