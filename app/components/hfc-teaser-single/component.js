04769f88a13219117c51d7687afe67670722fe1a
Nils Dannemann <ndannemann@gmail.com>
2016-06-01 08:54:55 +0200

    [refactor] removed old components

--- app/components/hfc-teaser-single/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  // teaserData: {},
-  // title: Ember.computed.alias('teaserData.title'),
-  // image: Ember.computed.alias('teaserData.image'),
-  // content: Ember.computed.alias('teaserData.content'),
-  // link: Ember.computed.alias('teaserData.link'),
-});
