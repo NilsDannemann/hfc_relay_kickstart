8e611dfa7b8be21b938bced19d60cc74013b31c3
Nils Dannemann <ndannemann@gmail.com>
2016-06-01 11:57:14 +0200

    [refactor] hfc-forms component

--- tests/integration/components/hfc-form/hfc-form-radiobutton/component-test.js
+++ tests/integration/components/hfc-form/hfc-form-radiobutton/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('hfc-teaser-double', 'Integration | Component | hfc teaser double', {
+moduleForComponent('hfc-form/hfc-form-radiobutton', 'Integration | Component | hfc form/hfc form radiobutton', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{hfc-teaser-double}}`);
+  this.render(hbs`{{hfc-form/hfc-form-radiobutton}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#hfc-teaser-double}}
+    {{#hfc-form/hfc-form-radiobutton}}
       template block text
-    {{/hfc-teaser-double}}
+    {{/hfc-form/hfc-form-radiobutton}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
