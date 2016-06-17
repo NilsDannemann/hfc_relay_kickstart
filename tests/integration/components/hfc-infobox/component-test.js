6c53a70d9d0833247638b73d82bc5c5aa0a3e1a8
Nils Dannemann <ndannemann@gmail.com>
2016-06-17 16:34:33 +0200

    [refactor] hfc-infobox

--- tests/integration/components/hfc-infobox/component-test.js
+++ tests/integration/components/hfc-infobox/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('sidebar-infobox-download', 'Integration | Component | hfc infobox download', {
+moduleForComponent('hfc-infobox', 'Integration | Component | hfc infobox', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{sidebar-infobox-download}}`);
+  this.render(hbs`{{hfc-infobox}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#sidebar-infobox-download}}
+    {{#hfc-infobox}}
       template block text
-    {{/sidebar-infobox-download}}
+    {{/hfc-infobox}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- tests/integration/components/hfc-infobox/hfc-infobox-list/component-test.js
+++ tests/integration/components/hfc-infobox/hfc-infobox-list/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('sidebar-infobox-list', 'Integration | Component | hfc infobox list', {
+moduleForComponent('hfc-infobox/hfc-infobox-list', 'Integration | Component | hfc infobox/hfc infobox list', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{sidebar-infobox-list}}`);
+  this.render(hbs`{{hfc-infobox/hfc-infobox-list}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#sidebar-infobox-list}}
+    {{#hfc-infobox/hfc-infobox-list}}
       template block text
-    {{/sidebar-infobox-list}}
+    {{/hfc-infobox/hfc-infobox-list}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
