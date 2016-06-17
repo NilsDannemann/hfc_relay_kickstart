a597921ea1779b8d6f0f9f9e5c4666414e71dd04
Nils Dannemann <ndannemann@gmail.com>
2016-06-17 15:43:05 +0200

    [refactor] gallery-image component model layer

--- app/components/hfc-gallery/hfc-gallery-image/component.js
+++ app/components/hfc-gallery/hfc-gallery-image/component.js
@@ -5,7 +5,18 @@ export default Ember.Component.extend({
   classNames: 'hfc-gallery-image',
 
   attributeBindings: ['_galleryImage:style', '_galleryImageLink:href', '_galleryImageTarget:target'],
-  _galleryImage: `background-image: url('assets/images/placeholder1.png');`,
-  _galleryImageLink: `assets/images/placeholder1.png`,
+
+  _galleryImage: Ember.computed('image', function(){
+    let image = this.get('image');
+
+    if (image) {
+      return `background-image: url('assets/${image}');`;
+    } else {
+      return `background-image: url('assets/images/placeholder1.png');`;
+    }
+  }),
+  _galleryImageLink: Ember.computed('image', function(){
+    return `assets/${this.get('image')}`;
+  }),
   _galleryImageTarget: `_blank`,
 });
